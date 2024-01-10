e5aaf78731c4cfe98313a8dc5a865e9448c47164
METE OMER <155827820+omermetetop@users.noreply.github.com>
2024-01-10 19:08:13 +0300

    Add files via upload

---
+++ p.py
@@ -0,0 +1,30 @@
+adi,yaş,sehir=("mete","16","karabük")
+print(yaş)
+print(adi)
+print(sehir)
+#
+sayilar="10"
+sayilar1="20"
+print(sayilar+sayilar1)
+#
+x=0.2
+y=34409
+maas=(y-(y*x))
+print(maas)
+#
+sayi=7
+print("mutlak değer:",abs(sayi))
+#
+us=pow(2,3)
+print("sayı üssü ",us)
+#
+from math import sqrt,ceil,floor
+sayi=25
+karekok=sqrt(sayi)
+print("sayının karekökü",karekok)
+#
+ondalik=10.25
+yukari=ceil(ondalik)
+asagi=floor(ondalik)
+print("yukarı",yukari)
+print("aşağı",asagi)
