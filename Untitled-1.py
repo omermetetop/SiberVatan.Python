a14e94cc69192a92d8e2f7f02748b9a67d582a12
METE OMER <155827820+omermetetop@users.noreply.github.com>
2024-01-10 19:09:09 +0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,21 @@
+def veri(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def asal(input_str):
+    if input_str.isdigit():
+        number = int(input_str)
+        if veri(number):
+            print(f"{number} sasaldır.")
+        else:
+            print(f"{number}  asal değildir.")
+    else:
+        print("metinsel.")
+
+# Kullanıcıdan input al
+user_input = input("giriş: ")
+asal(user_input)
